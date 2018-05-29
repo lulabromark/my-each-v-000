71b0fa12aef883a4111c486bf39fcf973bf87624
Mark Lawrence <lawrence.ml@gmail.com>
2018-05-29 00:21:55 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,12 +1,14 @@
 def my_each(array)
+  # code here
   i = 0
 
   while i < array.length
     yield array[i]
     i += 1
   end
-  array
+  return array
 end
 
 collection = [1, 2, 3, 4]
-my_each(collection) {|i|}
+my_each(collection) do |i|
+end
